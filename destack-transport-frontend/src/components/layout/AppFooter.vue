1756301ac9ad55d9a3e09dd241279a89de6373d6
ITALO DA SILVA COSTA <italosilva14@hotmail.com>
2025-11-21 21:39:00 -0300

    Delete destack-transport-frontend directory

--- destack-transport-frontend/src/components/layout/AppFooter.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-footer app density="compact" class="justify-center">
-    <span>© {{ new Date().getFullYear() }} Destack Sistemas. Todos os direitos reservados.</span>
-  </v-footer>
-</template>
